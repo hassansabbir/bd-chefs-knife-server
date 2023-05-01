82b643cdf6330d7e5b15d32aaf207e7b5818efc0
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-05-02 05:57:13 +0600

    json file send to surver

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chefsData = require("./data/chefs.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chefs Knife is running...");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefsData);
 });
 
 app.listen(port, () => {
