e8b3e5a09bd7775fcca78390725ee91cc9b1d872
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-05-02 05:50:46 +0600

    cors file added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chefs Knife is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Chefs knife is running on port: ${port}`);
 });
